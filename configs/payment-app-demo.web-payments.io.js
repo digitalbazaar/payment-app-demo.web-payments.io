bc4bb12739d83c412bbbd2a9e8b58a4ca3a0e0e4
Manu Sporny <msporny@digitalbazaar.com>
2016-04-30 15:54:03 -0400

    Enable directory chowning in production.

--- configs/payment-app-demo.web-payments.io.js
+++ configs/payment-app-demo.web-payments.io.js
@@ -32,6 +32,9 @@ config.loggers.access.filename = path.join(
 config.loggers.error.filename = path.join(
   _logdir, 'payment-app-demo.web-payments.io-error.log');
 config.loggers.email.silent = true;
+config.loggers.app.bedrock.enableChownDir = true;
+config.loggers.app.bedrock.enableChownDir = true;
+config.loggers.error.bedrock.enableChownDir = true;
 
 // server info
 config.server.port = 443;

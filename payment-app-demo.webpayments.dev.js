26a48e7b99004b8f1cf9c78cb6d7d79385b1249b
Manu Sporny <msporny@digitalbazaar.com>
2016-04-29 20:42:06 -0400

    Add base implementation files.

---
+++ payment-app-demo.webpayments.dev.js
@@ -0,0 +1,16 @@
+/*
+ * payment-app-demo.web-payments.io development server starter.
+ *
+ * Copyright (c) 2016 Digital Bazaar, Inc. All rights reserved.
+ */
+var bedrock = require('bedrock');
+
+require('./lib/main');
+require('./configs/dev');
+
+// configure for tests
+bedrock.events.on('bedrock.test.configure', function() {
+  require('./configs/test');
+});
+
+bedrock.start();
